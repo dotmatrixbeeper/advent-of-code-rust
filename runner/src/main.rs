8682dca9d2cb11b13f1a6c2397bf15f4fb90be82
Karuna Kiran Bhadra <scriptface@proton.me>
2026-02-02 23:38:04 +0530

    2016 day 11 finished

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, path::PathBuf};
 
-const YEARS_DAYS: [(u16, (u16, u16)); 2] = [(2015, (1, 25)), (2016, (1, 10))];
+const YEARS_DAYS: [(u16, (u16, u16)); 2] = [(2015, (1, 25)), (2016, (1, 11))];
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -63,6 +63,7 @@ fn run_specific_day(year: u16, day: u16) {
         (2016, 8) => solutions::y2016::day08::run(&input),
         (2016, 9) => solutions::y2016::day09::run(&input),
         (2016, 10) => solutions::y2016::day10::run(&input),
+        (2016, 11) => solutions::y2016::day11::run(&input),
         _ => {
             eprintln!("No solution avaliable for {}/{}", year, day);
             std::process::exit(1);
